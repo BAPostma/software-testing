0aff0b52fb779a32acd43f862920fe7281e1062c
Kevin van Cleef <kevin@developmentstudio.nl>
2013-09-08 18:27:43 +0200

    Directory reorganisation, please read README.md

--- week1/Barry Postma/Tutorial.hs
+++
@@ -1,9 +0,0 @@
-doubleMe :: Int -> Int
-doubleMe x = x + x
-
-doubleUs :: Int -> Int -> Int
-doubleUs x y = doubleMe x + doubleMe y
-
-removeUpperCase :: String -> String
-removeUpperCase str = [ x | x <- str, not (elem x ['A'..'Z']) ]
-

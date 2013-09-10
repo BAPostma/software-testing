95bc739a02acf7ff2d761b4b43786db509467e30
leonardpunt <leonard@q42.nl>
2013-09-10 12:43:47 +0200

    adds test functions

--- Group/week_2_group_GR_D2_2/Exercise3.hs
+++ Group/week_2_group_GR_D2_2/Exercise3.hs
@@ -23,3 +23,19 @@ dist' :: Form -> Form -> Form
 dist' (Cnj fs) q = Cnj (map (\p -> dist' p q) fs)
 dist' p (Cnj fs) = Cnj (map (dist' p) fs)
 dist' p q = Dsj [p, q]
+
+-- test functions
+isCNF :: Form -> Bool
+isCNF (Cnj fs) = and (map isCNF fs)
+isCNF (Dsj fs) = and (map isDist fs)
+isCNF f = isLiteral f
+
+isDist :: Form -> Bool
+isDist (Cnj _) = False
+isDist (Dsj fs) = and (map isDist fs)
+isDist f = isLiteral f
+
+isLiteral :: Form -> Bool
+isLiteral (Prop x) = True
+isLiteral (Neg (Prop x)) = True
+isLiteral _ = False

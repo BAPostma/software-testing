3fd94fe782af64efa7806fef10ff6d1f8637d04f
leonardpunt <leonard@q42.nl>
2013-09-11 19:51:14 +0200

    Adds shorter implementation for exercise 1

--- Individual/Week2/Leonard Punt/Exercise1.hs
+++ Individual/Week2/Leonard Punt/Exercise1.hs
@@ -18,10 +18,22 @@ triangle a b c | a + b <= c || a + c <= b || b + c <= a                   = NoTr
 
 {-
   A way to improve this function might be to have a precondition that a, b and c are sorted. So a <= b and b <= c, because:
-  - This will shorten the implementation of Isosceles, we will only have to check if a == b || b == c. The check for a == c will be redundant because if that is true, it has to be an Equilateral, which we already covered in the case above.
-  - The check for NoTriangle will also become easier, we will only have to check if a + b <= c. Because if the two smallest sides are greater than the biggest side, than 1 small side + 1 big side will always be bigger than the other smaller side.
+  - It will make fhe check for NoTriangle easier, we will only have to check if a + b <= c. Because if the two smallest sides are greater than the biggest side, than 1 small side + 1 big side will always be bigger than the other smaller side.
+  - It will shorten the implementation of Isosceles, we will only have to check if a == b || b == c. The check for a == c will be redundant because if that is true, it has to be an Equilateral, which we already covered in the case above.
+  - It will shorten the implementation of Rectangular, because we only have to check if a^2 + b^2 = c^2.
 -}
+
+triangle1 :: Integer -> Integer -> Integer -> Shape
+triangle1 a b c = triangle1' (xs !! 0) (xs !! 1) (xs !! 2) where xs = sort [a, b, c]
+
+triangle1' :: Integer -> Integer -> Integer -> Shape
+triangle1' a b c | a + b <= c       = NoTriangle
+                 | a == b && b == c = Equilateral
+                 | a == b || b == c = Isosceles
+                 | a^2 + b^2 == c^2  = Rectangular
+                 | otherwise        = Other
 
 -- test functions
 triples = [[x, y, z] | x <- [0..5], y <- [x..5], z <- [y..5]]
 test shape = [[x, y, z] | [x, y, z] <- triples, triangle x y z == shape]
+test1 shape = [[x, y, z] | [x, y, z] <- triples, triangle1 x y z == shape]

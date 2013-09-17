b78fa6a3e4d7bfbf1cb5ac9c5944b1f557822a63
leonardpunt <leonard@q42.nl>
2013-09-17 16:06:35 +0200

    adds exercise 7

--- Individual/Week3/Leonard Punt/Lab3.hs
+++ Individual/Week3/Leonard Punt/Lab3.hs
@@ -1,9 +1,11 @@
 module Lab3 where
 
 import Week2
+import Week3
 import Exercise3Improved
 import Techniques
 import System.Random
+import Data.Char
 import Data.List
 import Data.Ord
 
@@ -61,3 +63,103 @@ testCNF n (f:fs) =
   then do print ("pass on: " ++ show f)
           testCNF n fs
   else error ("failed test on: " ++ show f)
+
+-- Exercise 7
+-- Note: because 'A' and 'E' are 'reserved' for Forall and Exists, this functions generates a random char between 'f' and 'z'
+getRandomChar :: IO Char
+getRandomChar = getStdRandom (randomR ('f', 'z'))
+
+getRandomTermName :: IO String
+getRandomTermName = do
+  c <- getRandomChar
+  return [c]
+
+getRandomFormulaName :: IO String
+getRandomFormulaName = do
+  c <- getRandomChar
+  return [toUpper c]
+
+getRandomT :: IO Term
+getRandomT = do
+  d <- getRandomInt 4
+  getRandomTerm d
+
+getRandomTerm :: Int -> IO Term
+getRandomTerm 0 = do
+  m <- getRandomTermName
+  return (V m)
+getRandomTerm d = do
+  n <- getRandomInt 1
+  case n of
+    0 -> do m <- getRandomTermName
+            return (V m)
+    1 -> do m <- getRandomTermName
+            i <- getRandomInt 5
+            ts <- getRandomTerms (d-1) i
+            return (F m ts)
+
+getRandomTs :: Int -> IO [Term]
+getRandomTs n = do
+  d <- getRandomInt 3
+  getRandomTerms d n
+
+getRandomTerms :: Int -> Int -> IO [Term]
+getRandomTerms _ 0 = return []
+getRandomTerms d n = do
+  t <- getRandomTerm d
+  ts <- getRandomTerms d (n-1)
+  return (t:ts)
+
+getRandomFormula :: IO Formula
+getRandomFormula = do
+  d <- getRandomInt 4
+  getRandomFormula' d
+
+getRandomFormula' :: Int -> IO Formula
+getRandomFormula' 0 = do
+  m <- getRandomFormulaName
+  i <- getRandomInt 3
+  ts <- getRandomTs i
+  return (Atom m ts)
+getRandomFormula' d = do
+  n <- getRandomInt 8
+  case n of
+    0 -> do m <- getRandomFormulaName
+            i <- getRandomInt 3
+            fs <- getRandomTs i
+            return (Atom m fs)
+    1 -> do t1 <- getRandomT
+            t2 <- getRandomT
+            return (Eq t1 t2)
+    2 -> do f <- getRandomFormula' (d-1)
+            return (Week3.Neg f)
+    3 -> do f1 <- getRandomFormula' (d-1)
+            f2 <- getRandomFormula' (d-1)
+            return (Week3.Impl f1 f2)
+    4 -> do f1 <- getRandomFormula' (d-1)
+            f2 <- getRandomFormula' (d-1)
+            return (Equi f1 f2)
+    5 -> do i <- getRandomInt 5
+            fs <- getRandomFormulas' (d-1) i
+            return (Conj fs)
+    6 -> do i <- getRandomInt 5
+            fs <- getRandomFormulas' (d-1) i
+            return (Disj fs)
+    7 -> do m <- getRandomFormulaName
+            f <- getRandomFormula' (d-1)
+            return (Forall m f)
+    8 -> do m <- getRandomFormulaName
+            f <- getRandomFormula' (d-1)
+            return (Exists m f)
+
+getRandomFormulas :: Int -> IO [Formula]
+getRandomFormulas n = do
+  d <- getRandomInt 3
+  getRandomFormulas' d n
+
+getRandomFormulas' :: Int -> Int -> IO [Formula]
+getRandomFormulas' _ 0 = return []
+getRandomFormulas' d n = do
+  f <- getRandomFormula' d
+  fs <- getRandomFormulas' d (n-1)
+  return (f:fs)

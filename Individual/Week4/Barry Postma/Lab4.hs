a966f43be8b1e9dec1080880609cf114d25e6882
BAPostma <barry.postma@student.uva.nl>
2013-09-23 16:15:14 +0200

    Added exercise 3.1

--- Individual/Week4/Barry Postma/Lab4.hs
+++ Individual/Week4/Barry Postma/Lab4.hs
@@ -2,6 +2,7 @@ module Lab4
 
 where
 
+import Data.List
 import SetOrd
 import Week3
 
@@ -14,3 +15,9 @@ randomSet = do
 			intl <- getIntList'
 			return (list2set intl)
 			
+{-|
+	Exercise 3.1
+-}
+intersection :: Eq a => (Set a) -> (Set a) -> (Set a)
+intersection (Set s1) (Set s2) = Set (intersect s1 s2)
+

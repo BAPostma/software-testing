094123b67350914cf035dec91b453782e63c891c
BAPostma <barry.postma@student.uva.nl>
2013-09-24 11:38:12 +0200

    Test for exercise 3's intersection and union'

--- Individual/Week4/Barry Postma/Lab4.hs
+++ Individual/Week4/Barry Postma/Lab4.hs
@@ -31,3 +31,23 @@ union' (Set s1) (Set s2) = Set (Data.List.union s1 s2)
 diff :: Eq a => (Set a) -> (Set a) -> (Set a)
 diff (Set s1) (Set s2) = union' (Set (s1 \\ s2)) (Set (s2 \\ s1))
 
+{-|
+	Exercise 3 - TESTS
+-}
+-- 3.4.1 Testing of intersection and union' at once.
+-- Every run of this function should return a set containing element 100
+testIntersection :: IO (Set Int)
+testIntersection = do
+				   testSet1 <- randomSet
+				   testSet2 <- randomSet
+				   let testControlSet1 = union' testSet1 (Set [99,100,101])
+				   let testControlSet2 = union' testSet2 (Set [100])
+				   let testSetUnions = (intersection testControlSet1 testControlSet2)
+				   return testSetUnions
+
+
+
+
+
+
+

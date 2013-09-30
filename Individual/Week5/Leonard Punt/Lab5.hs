fbb38c48a80b7148f5b47eedef78c05c61e12d97
leonardpunt <leonard@q42.nl>
2013-09-30 15:22:54 +0200

    adds exercise 2

--- Individual/Week5/Leonard Punt/Lab5.hs
+++ Individual/Week5/Leonard Punt/Lab5.hs
@@ -12,3 +12,22 @@ mergeSrt (x:xs) = merge [x] (mergeSrt xs)
 -- Assertion: every element in the result list should be smaller or equal than all its succeeding items
 mergeSrtA :: Ord a => [a] -> [a]
 mergeSrtA = assert1 (\ _ ys -> sorted ys) mergeSrt
+
+-- Exercise 2 - Time spent: 20 minutes
+split :: [a] -> ([a],[a])
+split xs = let
+   n = (length xs) `div` 2
+  in
+   (take n xs, drop n xs)
+
+mergeSrt1 :: Ord a => [a] -> [a]
+mergeSrt1 [] = []
+mergeSrt1 [x] = [x]
+mergeSrt1 xs = let
+   (ys, zs) = split xs
+  in
+   merge (mergeSrt1 ys) (mergeSrt1 zs)
+
+-- Assertion: every element in the result list should be smaller or equal than all its succeeding items
+mergeSrt1A :: Ord a => [a] -> [a]
+mergeSrt1A = assert1 (\ _ ys -> sorted ys) mergeSrt1

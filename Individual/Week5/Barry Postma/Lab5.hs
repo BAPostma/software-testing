be282c6695a7417843a4869a4521bc2b9eaa9489
BAPostma <barry.postma@student.uva.nl>
2013-09-30 15:19:00 +0200

    Added exercise 1

--- Individual/Week5/Barry Postma/Lab5.hs
+++ Individual/Week5/Barry Postma/Lab5.hs
@@ -5,3 +5,21 @@ where
 import Data.List
 import Week5
 
+{-|
+	Exercise 1
+-}
+-- Given
+mergeSrt :: Ord a => [a] -> [a]
+mergeSrt [] = []
+mergeSrt (x:xs) = merge [x] (mergeSrt xs)
+
+-- Assertion wrapper
+mergeSrt' :: Ord a => ([a] -> [a] -> Bool) -> [a] -> [a]
+mergeSrt' p xs = if p xs (mergeSrt xs) then mergeSrt xs
+			     else error "pre-/postcondition not met"
+
+-- Usage of the assertion
+-- Precondition: None
+-- Postcondition: values are sorted in ascending order
+mergeSrtAssertionResult = mergeSrt' (\ _ (x:y:xs) -> x < y) [10,5,100,60,40,77]
+
